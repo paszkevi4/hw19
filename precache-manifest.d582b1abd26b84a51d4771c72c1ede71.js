40731241af45746d95383d6641d2398d77b591d9
paszkevi4 <paszkevi4@gmail.com>
2020-09-16 23:03:54 +0300

    Updates

---
+++ precache-manifest.d582b1abd26b84a51d4771c72c1ede71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa3e35dfd84482a8b361c55e62a2436",
+    "url": "/hw19/index.html"
+  },
+  {
+    "revision": "4cdc3a77b66babfb3d2c",
+    "url": "/hw19/static/css/main.47135346.chunk.css"
+  },
+  {
+    "revision": "80a8a13730ffe5476bc7",
+    "url": "/hw19/static/js/2.7764dac6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hw19/static/js/2.7764dac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cdc3a77b66babfb3d2c",
+    "url": "/hw19/static/js/main.c09e1a0a.chunk.js"
+  },
+  {
+    "revision": "075e8075a2f30303e7b8",
+    "url": "/hw19/static/js/runtime-main.ced06f0c.js"
+  }
+]);
